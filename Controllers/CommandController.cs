59f07f943411c8f263927b5194e3fb3d1b0b1df2
João Paulo <jpresendejava@gmail.com>
2024-04-09 16:27:07 -0400

    seach command to id

--- Controllers/CommandController.cs
+++ Controllers/CommandController.cs
@@ -75,5 +75,47 @@ namespace Restaurant_table_control_api.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
+    
+        [HttpGet("{id}")]
+public async Task<IActionResult> JsonCommand(int id)
+{
+    try
+    {
+        var lista = await _context.Commands_Entity
+            .Where(a => a.Id == id)
+            .Include(p => p.productDatails)
+            .Include(t => t.restaurantTable)
+            .Select(c => new
+            {
+                c.Id,
+                c.Description,
+                restaurantTable = c.restaurantTable != null ? c.restaurantTable.Description : "",
+                productDetails = c.productDatails.Select(pd => new
+                {
+                    pd.Id,
+                    pd.Barcode,
+                    pd.Description,
+                    pd.Qtd,
+                    pd.Sale_Price,
+                    TotalPrice = pd.Qtd * pd.Sale_Price, // Multiplicação da quantidade pelo preço de venda
+                    pd.date_Of_Sale,
+                    pd.commandId
+                }),
+                Total_Command = c.productDatails.Sum(pd => pd.Qtd * pd.Sale_Price)
+            })
+            .ToListAsync();
+
+        return Ok(lista);
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, $"Internal server error: {ex.Message}");
+    }
+}
+
+    
+    
+    
+    
     }
 }
